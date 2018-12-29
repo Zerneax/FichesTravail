d70881ae79f0d95bef500c9fe9161dce5f007e40
Zerneax <d.chris59@gmail.com>
2018-12-29 21:31:46 +0100

    refactor

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import jsPDF from 'jspdf';
 import { HomeService } from '../services/home/home.service';
 
 @Component({
@@ -39,20 +38,29 @@ export class HomeComponent implements OnInit {
 
   generateCalculations() {
 
-    if(this.generateForm.value['nbAdd'] != undefined) {
-      this.generateAdds();
+    let minA = this.generateForm.value['minA'];
+    let maxA = this.generateForm.value['maxA'];
+    let minB = this.generateForm.value['minB'];
+    let maxB = this.generateForm.value['maxB'];
+    let nbAdd = this.generateForm.value['nbAdd'];
+    let nbSub = this.generateForm.value['nbSub'];
+    let nbMul = this.generateForm.value['nbMul'];
+    let nbDiv = this.generateForm.value['nbDiv'];
+
+    if( nbAdd != undefined) {
+      this.calculations = this.calculations.concat(this.homeService.generateAdds(nbAdd, minA, maxA, minB, maxB));
     }
 
-    if(this.generateForm.value['nbSub'] != undefined) {
-      this.generateSubs();
+    if( nbSub != undefined) {
+      this.calculations = this.calculations.concat(this.homeService.generateSubs(nbSub, minA, maxA, minB, maxB));
     }
 
-    if(this.generateForm.value['nbMul'] != undefined) {
-      this.generateMuls();
+    if( nbMul != undefined) {
+      this.calculations = this.calculations.concat(this.homeService.generateMuls(nbMul, minA, maxA, minB, maxB));
     }
 
-    if(this.generateForm.value['nbDiv'] != undefined) {
-      this.generateDivs();
+    if( nbDiv != undefined) {
+      this.calculations = this.calculations.concat(this.homeService.generateDivs(nbDiv, minA, maxA, minB, maxB));
     }
 
     for(var i = 0; i < this.calculations.length; i ++) {
@@ -60,114 +68,8 @@ export class HomeComponent implements OnInit {
     }
   }
 
-  generateAdds() {
-
-    for(var i = 0; i < this.generateForm.value['nbAdd'];) {
-      let a = this.homeService.generateRandomNumber(this.generateForm.value['minA'], this.generateForm.value['maxA']);
-      let b = this.homeService.generateRandomNumber(this.generateForm.value['minB'], this.generateForm.value['maxB']);
-      let calculation = '' + a + ' + ' + b + " = ______";
-      if(!this.calculations.includes(calculation)) {
-        this.calculations.push(calculation);
-        i ++;
-      }
-
-    }
-  }
-
-  generateSubs() {
-
-    for(var i = 0; i < this.generateForm.value['nbSub'];) {
-      let a = this.homeService.generateRandomNumber(this.generateForm.value['minA'], this.generateForm.value['maxA']);
-      let b = this.homeService.generateRandomNumber(this.generateForm.value['minB'], this.generateForm.value['maxB']);
-      let calculation = '' + a + ' - ' + b + " = ______";
-      if(!this.calculations.includes(calculation)) {
-        this.calculations.push(calculation);
-        i ++;
-      }
-    }
-  }
-
-  generateMuls() {
-
-    for(var i = 0; i < this.generateForm.value['nbMul'];) {
-      let a = this.homeService.generateRandomNumber(this.generateForm.value['minA'], this.generateForm.value['maxA']);
-      let b = this.homeService.generateRandomNumber(this.generateForm.value['minB'], this.generateForm.value['maxB']);
-      let calculation = '' + a + ' x ' + b + " = ______";
-      if(!this.calculations.includes(calculation)) {
-        this.calculations.push(calculation);
-        i ++;
-      }
-    }
-  }
-
-  generateDivs() {
-
-    for(var i = 0; i < this.generateForm.value['nbDiv'];) {
-      let a = this.homeService.generateRandomNumber(this.generateForm.value['minA'], this.generateForm.value['maxA']);
-      let b = this.homeService.generateRandomNumber(this.generateForm.value['minB'], this.generateForm.value['maxB']);
-      let calculation = '' + a + ' : ' + b + " = ______";
-      if(!this.calculations.includes(calculation)) {
-        this.calculations.push(calculation);
-        i ++;
-      }
-    }
-  }
-
-
-
   generatePDF() {
-
-    var nbPages = 1;
-
-    var maxPages = 1;
-    if(this.calculations.length > 24  )
-      maxPages = Math.round(this.calculations.length / 24);
-
-    var doc = new jsPDF();
-
-    doc.setLineWidth(1);
-    doc.rect(5, 5, 100, 25);
-    doc.rect(105, 5, 100, 25);
-    doc.setFontStyle("bold");
-    doc.setFontSize(22);
-    doc.text('Calculs', 40, 20);
-
-    doc.setFontStyle("normal");
-    doc.setFontSize(14);
-    var x = 20;
-    var y = 50;
-
-    for(var i = 0; i < this.calculations.length; i ++) {
-      doc.text(this.calculations[i], x, y);
-      x += 60;
-
-      if(i != 0 && i % 23 == 0) {
-        x = 20;
-        y = 50;
-        doc.text('Page ' + nbPages + '/' + maxPages, 180, 290);
-        nbPages ++;
-
-        doc.addPage(nbPages, 'a6');
-        doc.setLineWidth(1);
-        doc.rect(5, 5, 100, 25);
-        doc.rect(105, 5, 100, 25);
-        doc.setFontStyle("bold");
-        doc.setFontSize(22);
-        doc.text('Calculs', 40, 20);
-
-        doc.setFontStyle("normal");
-        doc.setFontSize(14);
-      }
-
-      if(x > 140) {
-        x = 20;
-        y += 30;
-      }
-
-    }
-
-    doc.text('Page ' + nbPages + '/' + maxPages, 180, 290);
-    doc.save('Fiche calculs.pdf');
+    this.homeService.generatePDF(this.calculations);
   }
 
   submit() {
--- src/app/services/home/home.service.ts
+++ src/app/services/home/home.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import jsPDF from 'jspdf';
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +10,118 @@ export class HomeService {
 
   generateRandomNumber(min, max) {
     return Math.ceil((Math.random() * max) + min);
+  }
+
+  generateAdds(nbAdd, minA, maxA, minB, maxB) {
+    let calculations: Array<String> = [];
+    for(var i = 0; i < nbAdd;) {
+      let a = this.generateRandomNumber(minA, maxA);
+      let b = this.generateRandomNumber(minB, maxB);
+      let calculation = '' + a + ' + ' + b + " = ______";
+      if(!calculations.includes(calculation)) {
+        calculations.push(calculation);
+        i ++;
+      }
+
+    }
+    return calculations;
+  }
+
+  generateSubs(nbSub, minA, maxA, minB, maxB) {
+    let calculations : Array<String> = [];
+    for(var i = 0; i < nbSub;) {
+      let a = this.generateRandomNumber(minA, maxA);
+      let b = this.generateRandomNumber(minB, maxB);
+      let calculation = '' + a + ' - ' + b + " = ______";
+      if(!calculations.includes(calculation)) {
+        calculations.push(calculation);
+        i ++;
+      }
+    }
+    return calculations;
+  }
+
+  generateMuls(nbMul, minA, maxA, minB, maxB) {
+    let calculations : Array<String> = [];
+    for(var i = 0; i < nbMul;) {
+      let a = this.generateRandomNumber(minA, maxA);
+      let b = this.generateRandomNumber(minB, maxB);
+      let calculation = '' + a + ' x ' + b + " = ______";
+      if(!calculations.includes(calculation)) {
+        calculations.push(calculation);
+        i ++;
+      }
+    }
+    return calculations;
+  }
+
+
+  generateDivs(nbDiv, minA, maxA, minB, maxB) {
+    let calculations : Array<String> = [];
+    for(var i = 0; i < nbDiv;) {
+      let a = this.generateRandomNumber(minA, maxA);
+      let b = this.generateRandomNumber(minB, maxB);
+      let calculation = '' + a + ' : ' + b + " = ______";
+      if(!calculations.includes(calculation)) {
+        calculations.push(calculation);
+        i ++;
+      }
+    }
+    return calculations;
+  }
+
+  generatePDF(calculations) {
+
+    var nbPages = 1;
+
+    var maxPages = 1;
+    if(calculations.length > 24  )
+      maxPages = Math.round(calculations.length / 24);
+
+    var doc = new jsPDF();
+
+    doc.setLineWidth(1);
+    doc.rect(5, 5, 100, 25);
+    doc.rect(105, 5, 100, 25);
+    doc.setFontStyle("bold");
+    doc.setFontSize(22);
+    doc.text('Calculs', 40, 20);
+
+    doc.setFontStyle("normal");
+    doc.setFontSize(14);
+    var x = 20;
+    var y = 50;
+
+    for(var i = 0; i < calculations.length; i ++) {
+      doc.text(calculations[i], x, y);
+      x += 60;
+
+      if(i != 0 && i % 23 == 0) {
+        x = 20;
+        y = 50;
+        doc.text('Page ' + nbPages + '/' + maxPages, 180, 290);
+        nbPages ++;
+
+        doc.addPage(nbPages, 'a6');
+        doc.setLineWidth(1);
+        doc.rect(5, 5, 100, 25);
+        doc.rect(105, 5, 100, 25);
+        doc.setFontStyle("bold");
+        doc.setFontSize(22);
+        doc.text('Calculs', 40, 20);
+
+        doc.setFontStyle("normal");
+        doc.setFontSize(14);
+      }
+
+      if(x > 140) {
+        x = 20;
+        y += 30;
+      }
+
+    }
+
+    doc.text('Page ' + nbPages + '/' + maxPages, 180, 290);
+    doc.save('Fiche calculs.pdf');
   }
 }
